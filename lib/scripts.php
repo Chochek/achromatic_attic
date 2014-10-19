da68b2b7645223571bb7edcb52c48660b175951c
Marko <marko.corokalo@gmail.com>
2014-10-19 19:06:16 +0200

    update

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function achromatic_attic_scripts() {
-  wp_enqueue_style('achromatic_attic_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1c251d84f4ed7610e20126ed024206d4');
+  wp_enqueue_style('achromatic_attic_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6bbde8f2f5e35f2fca189e8dfa560eab');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
